334a06b5ff10c20b07f196b1703bc324dcedfcf0
burakemreozer <burak.oezer@checktur.io>
2023-12-14 04:24:14 +0100

    init repo

---
+++ src/main/java/com/fitiz/fitnessmetrics/FitnessMetricsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fitiz.fitnessmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessMetricsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessMetricsServiceApplication.class, args);
+	}
+
+}
